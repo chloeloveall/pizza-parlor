72c871a693e48d32f0dca7da4cd71ce96db84413
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-01-29 15:31:26 -0800

    Pass test for prototype that create ID for each order

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
 // Business logic for Order ---------
 function Order() {
   this.pizzas = {};
+  this.currentOrderId = 0;
+}
+
+Order.prototype.assignOrderId = function() {
+  this.currentOrderId += 1;
+  return this.currentOrderId;
 }
 
 Order.prototype.addPizzaOrder = function(pizza) {
-  this.pizzas[pizza.orderName] = pizza;
+  pizza.orderId = this.assignOrderId();
+  this.pizzas[pizza.orderId] = pizza;
 }
 
 // Business logic for Pizza ---------
@@ -15,6 +22,9 @@ function Pizza(orderName, pizzaSize, toppings) {
   this.totalPizzaCost = 0;
 }
 
+let order = new Order();
+let pizza = new Pizza('Chloe', 'small', 'yes')
+order.addPizzaOrder(pizza)
 // Pizza.prototype.sizeSelect = function() {
   
 // }
