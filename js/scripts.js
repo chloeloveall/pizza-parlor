a388ed27576082c82559b5a57f2fa94e6c820ea8
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-01-29 14:32:35 -0800

    Pass test that addPizzaOrder stores instances of pizza in Order object

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
+// Business logic for Order ---------
+function Order() {
+  this.pizzas = {};
+}
+
+Order.prototype.addPizzaOrder = function(pizza) {
+  this.pizzas[pizza.orderName] = pizza;
+}
+
 // Business logic for Pizza ---------
-function Pizza() {
+function Pizza(orderName, pizzaSize, toppings) {
+  this.orderName = orderName;
   this.pizzaSize = pizzaSize;
   this.toppings = toppings;
   this.totalPizzaCost = 0;
 }
 
-Pizza.prototype.sizeSelect = function() {
+// Pizza.prototype.sizeSelect = function() {
   
-  if ()
-}
+// }
