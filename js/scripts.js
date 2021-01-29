de07b36c2c9ec9c0eea8af9a2cf69df57da777fb
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-01-29 14:09:16 -0800

    Pass test for Order constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 // Business logic for Pizza ---------
 function Pizza() {
-  this.pizzaSize = {}
-  this.toppings = {}
+  this.pizzaSize = pizzaSize;
+  this.toppings = toppings;
   this.totalPizzaCost = 0;
+}
+
+Pizza.prototype.sizeSelect = function() {
+  
+  if ()
 }
