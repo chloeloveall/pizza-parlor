6ec3b1c6d31e764da546c758a8875da1c94928ea
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-01-29 16:41:05 -0800

    Add jQuery doc ready and form submit, const for name input and pizza size input

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,13 @@ function Order() {
   this.currentOrderId = 0;
 }
 
+// Assigns unique ID each time a pizza object is added to Order
 Order.prototype.assignOrderId = function() {
   this.currentOrderId += 1;
   return this.currentOrderId;
 }
 
+// Add instances of Pizza to be stored in Order
 Order.prototype.addPizzaOrder = function(pizza) {
   pizza.orderId = this.assignOrderId();
   this.pizzas[pizza.orderId] = pizza;
@@ -25,6 +27,13 @@ function Pizza(orderName, pizzaSize, toppings) {
 let order = new Order();
 let pizza = new Pizza('Chloe', 'small', 'yes')
 order.addPizzaOrder(pizza)
-// Pizza.prototype.sizeSelect = function() {
-  
-// }
+
+// User Interface Logic ---------
+$(document).ready(function() {
+  $('form#order-form').submit(function(event) {
+    event.preventDefault();
+    const orderName = $('input#userName').val();
+    const inputtedPizzaSize = $('input:radio[name=pizzaSize]:checked').val();
+
+  })
+})
