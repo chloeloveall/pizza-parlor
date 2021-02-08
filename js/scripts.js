3d468b7e2f5273a428a9347e4356fc26e259f430
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-02-08 00:53:43 -0800

    Remove assigned value of this.totalPizzaCost, remove global variables, add all pizza prototypes, connect UI logic to Business logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,38 +2,65 @@
 function Order() {
   this.pizzas = {};
   this.currentOrderId = 0;
-}
+};
 
 // Assigns unique ID each time a pizza object is added to Order
 Order.prototype.assignOrderId = function() {
   this.currentOrderId += 1;
   return this.currentOrderId;
-}
+};
 
 // Add instances of Pizza to be stored in Order
 Order.prototype.addPizzaOrder = function(pizza) {
   pizza.orderId = this.assignOrderId();
   this.pizzas[pizza.orderId] = pizza;
-}
+};
 
 // Business logic for Pizza ---------
-function Pizza(orderName, pizzaSize, toppings) {
+function Pizza(orderName, pizzaSize, toppings, totalPizzaCost) {
   this.orderName = orderName;
   this.pizzaSize = pizzaSize;
   this.toppings = toppings;
-  this.totalPizzaCost = 0;
-}
+  this.totalPizzaCost = totalPizzaCost;
+};
 
-let order = new Order();
-let pizza = new Pizza('Chloe', 'small', 'yes')
-order.addPizzaOrder(pizza)
+Pizza.prototype.addSize = function() {
+  if (this.pizzaSize === 'large') {
+    return this.totalPizzaCost = 20;
+  } else if (this.pizzaSize === 'medium') {
+    return this.totalPizzaCost = 15;
+  } else {
+    return this.totalPizzaCost = 10;
+  }
+};
+
+Pizza.prototype.addToppings = function() { 
+  let toppingsPrice = 0;
+  this.toppings.forEach(function() {
+    toppingsPrice += 3;
+  })
+  return toppingsPrice;
+};
+
+Pizza.prototype.calculateTotalCost = function() {
+  return this.totalPizzaCost = this.addSize() + this.addToppings();
+};
 
 // User Interface Logic ---------
 $(document).ready(function() {
   $('form#order-form').submit(function(event) {
     event.preventDefault();
-    const orderName = $('input#userName').val();
-    const inputtedPizzaSize = $('input:radio[name=pizzaSize]:checked').val();
+    const orderName = $('input#orderName').val();
+    const pizzaSize = $('input:radio[name=pizzaSize]:checked').val(); 
+    const toppings = [];
+    $('input:checkbox[name=topping]:checked').each(function() {
+      toppings.push($(this).val());
+    })
+    const pizza = new Pizza(orderName, pizzaSize, toppings);
+    const order = new Order();
+    order.addPizzaOrder(pizza);
 
-  })
-})
+    console.log(pizza);
+    console.log(pizza.calculateTotalCost());
+  });
+});
